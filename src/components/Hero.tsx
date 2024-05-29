54c425a5d67a0dc05306a1e89f25502b0add133b
Edem <edem@Edems-MacBook-Pro.local>
2024-05-30 01:16:34 +0200

    adding spotlight

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ export const Hero = () => {
     return (
         <div className="pb-20 pt-36">
             <div>
-                <h1 className="text-white">Hello, Portfolio</h1>
+                <h1 className="text-white">Hello, Portfolio of Edem</h1>
             </div>
         </div>
     )
