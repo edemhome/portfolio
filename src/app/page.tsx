b8628f60461cabf43e5f941f19ebc02250807373
Edem <edem@Edems-MacBook-Pro.local>
2024-05-30 10:18:28 +0200

    adding prettier and engine stric rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { Hero } from "@/components/Hero";
-import Image from "next/image";
+import { Hero } from '@/components/Hero';
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <main className="relative flex min-h-screen flex-col items-center justify-center p-24 bg-black-100 overflow-auto mx-auto sm:px10 px-5">
-      <div className="z-10 w-full max-w-7xl items-center justify-between font-mono text-sm lg:flex">
-        <Hero />
-      </div>
-    </main>
-  );
+    return (
+        <main className="relative flex min-h-screen flex-col items-center justify-center p-24 bg-black-100 overflow-auto mx-auto sm:px10 px-5">
+            <div className="z-10 w-full max-w-7xl items-center justify-between font-mono text-sm lg:flex">
+                <Hero />
+            </div>
+        </main>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,38 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        black: {
-          DEFAULT: '#000',
-          100: '#000319',
-        }
-
-      },
-      animation: {
-        spotlight: "spotlight 2s ease .75s 1 forwards",
-      },
-      keyframes: {
-        spotlight: {
-          "0%": {
-            opacity: 0,
-            transform: "translate(-72%, -62%) scale(0.5)",
-          },
-          "100%": {
-            opacity: 1,
-            transform: "translate(-50%,-40%) scale(1)",
-          },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                black: {
+                    DEFAULT: '#000',
+                    100: '#000319',
+                },
+            },
+            animation: {
+                spotlight: 'spotlight 2s ease .75s 1 forwards',
+            },
+            keyframes: {
+                spotlight: {
+                    '0%': {
+                        opacity: 0,
+                        transform: 'translate(-72%, -62%) scale(0.5)',
+                    },
+                    '100%': {
+                        opacity: 1,
+                        transform: 'translate(-50%,-40%) scale(1)',
+                    },
+                },
+            },
         },
-      },
-
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
