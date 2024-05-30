49b9d3dfbc55acb03335142ba9a1ac8af307c309
Edem <edem@Edems-MacBook-Pro.local>
2024-05-30 15:00:20 +0200

    adding themes provider

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,7 +17,14 @@ export const Hero = () => {
                     className="top-28 left-80 h-[80vh] w-[50vw]"
                     fill="blue"
                 />
-                <h1 className="text-white">Hello, Portfolio of Edem</h1>
+                <div className="h-[50rem] w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+                    {/* Radial gradient for the container to give a faded look */}
+                    <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+                    <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+                        Backgrounds
+                        <h1>Hello, Portfolio of Edem</h1>
+                    </p>
+                </div>
             </div>
         </div>
     );
